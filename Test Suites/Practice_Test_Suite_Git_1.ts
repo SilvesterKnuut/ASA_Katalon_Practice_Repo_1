90da5ea6562dc2080dfbab0f851a1f0ebd49cf4a
Silvester <Silvester@192.168.3.67>
2019-07-25 16:35:35 +0300

    Edited Test suite

--- Test Suites/Practice_Test_Suite_Git_1.ts
+++ Test Suites/Practice_Test_Suite_Git_1.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>eeed0335-9463-4ea7-8aad-940d9ca23298</testSuiteGuid>
+   <testCaseLink>
+      <guid>1482c891-d310-4cc0-8590-91d8da98f3ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_0_1_1_ASA_Practice_Git_TC</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
